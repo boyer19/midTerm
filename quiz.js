7fa0323ed128708b671325cbf1b817a5dc1d7858
Matt <ok7140mj@go.minnstate.edu>
2021-03-22 23:16:15 -0500

    Fixed null and enter, add credits

--- quiz.js
+++ quiz.js
@@ -1,13 +1,6 @@
 // API Courtesy of https://api.le-systeme-solaire.net/rest/bodies/ from https://api.le-systeme-solaire.net/en/
 
 
-// Things to ask Clara about-
-//     1- Why Enter button is not working
-//     2- Null -  not returning 0
-//     3- Error while returning to Home or another HTML
-//      4- Count for "celestial code" off, counting order in array. How did she get "country-code"?
-
-
 let playAgainButton = document.querySelector('#play-again-button')
 let userAnswerElement = document.querySelector("#user-answer")
 let resultTextElement = document.querySelector('#result') 
@@ -22,23 +15,23 @@ function celestialquiz() {
     let randomNumber = Math.floor(Math.random() * planetsAndDwarfs.length)
     console.log(randomNumber)
     
-    let maxFailedAttempts = 3                                    // Attempt count for failed connection
-    planet(maxFailedAttempts)                       //  Initial call function one time to start
+    let maxFailedAttempts = 3                                                   // Attempt count for failed connection
+    planet(maxFailedAttempts)                                                   //  Initial call function one time to start
 
-    planetsAndDwarfs[randomNumber]                    // Selects planet in random--square brackets= index of array
+    planetsAndDwarfs[randomNumber]                                              // Selects planet in random--square brackets= index of array
     console.log(planetsAndDwarfs[randomNumber])  
 
     let randomPlanetObject = planetsAndDwarfs[randomNumber] 
 
     randomPlanetElement.innerHTML = randomPlanetObject.englishName
 
-    let planetID = randomPlanetObject['id']                                             // Counting elements in planetsAndDwarfPlanets, not by "celestial-code"
+    let planetID = randomPlanetObject['id']                                     // Counting elements in planetsAndDwarfPlanets, not by "celestial-code"
     let url = `https://api.le-systeme-solaire.net/rest/bodies/${planetID}?format=json`
 
     console.log(planetsAndDwarfs)
 
-    document.addEventListener('keyup', function(e) {                     // enter button - not working
-        if(e.keycode === 13) {
+    document.addEventListener('keyup', function(e) {                     
+        if(e.key === 'Enter' || e.key === 13) {
             submitButton.click()
         }
        // console.log(e)
@@ -55,26 +48,22 @@ function celestialquiz() {
         return res.json()
     }).then( (moonData) => {
         console.log(moonData)
-    
-        // let moonInfo = moonData.moons 
-        // if (moonInfo.moons === '') {
-        //     moonData.moons == 0;
-        // } else {
-        //     return moonData.moons;
-        // }
 
-         let moonCount = moonData.moons.length
+        let moonArray = moonData.moons
+        let moonCount
+
+        if (moonArray == null) {
+            moonCount = 0;
+        } else {
+            moonCount = moonArray.length
+        }
+    
+        
                     
       
 
         submitButton.addEventListener('click', function() {
-            // let moonInfo = moonData.moons 
-            // if (moonData == null) {
-            //     moonInfo.moons = 0;
-            //    }  else {
-            //        return moonData.length;
-            //    }
-            //    console.log(moonInfo)
+            
             
             if (userAnswerElement.value == moonCount) { 
                 resultTextElement.innerHTML = "Correct"
@@ -90,7 +79,7 @@ function celestialquiz() {
 }
 
 
-playAgainButton.addEventListener('click', function() {                  // Error when going to another link without pressing "play again"
+playAgainButton.addEventListener('click', function() {                  
     //alert('The button was clicked to play again')
  
     userAnswerElement.value = ''
